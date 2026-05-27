2dc04a6c2b90228226e64fab5b6e4fb7bddf142c
Lương Trọng Hiếu <luongtronghieudz06@gmail.com>
2026-05-27 11:53:53 +0700

    [COMPONENT] Create About component

---
+++ session_04_react_basics/projects/portfolio_react/src/components/About.jsx
@@ -0,0 +1,12 @@
+function About() {
+    return (
+        <section id="about" className="about">
+            <div className="container">
+                <h2>About Me</h2>
+                <p>
+                    I am a passionate web developer with experience in building responsive and user-friendly websites. I enjoy learning new technologies and improving my skills to create better web experiences.
+                </p>
+            </div>
+        </section>
+    )
+}
