92df9ac7a509a4c350660f3eeb1d4d3a4d106d08
Lương Trọng Hiếu <luongtronghieudz06@gmail.com>
2026-05-27 12:03:53 +0700

    [COMPONENT] Create Contact component

---
+++ session_04_react_basics/projects/portfolio_react/src/components/Contact.jsx
@@ -0,0 +1,13 @@
+function Contact() {
+    return (
+        <section id="contact" className="contact">
+            <h2>Contact Me</h2>
+            <p>If you would like to get in touch, please feel free to reach out!</p>
+            <ul>
+                <li>Email: <a href="mailto:luongtronghieudz06@gmail.com">luongtronghieudz06@gmail.com</a></li>
+            </ul>
+        </section>
+    )
+}
+
+export default Contact;
