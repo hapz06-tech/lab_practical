b90f6eeb1300a4c3ad510c7650d1e7abbb971a05
Lương Trọng Hiếu <luongtronghieudz06@gmail.com>
2026-05-27 11:59:59 +0700

    [COMPONENT] Create Portfolio component

---
+++ session_04_react_basics/projects/portfolio_react/src/components/Portfolio.jsx
@@ -0,0 +1,10 @@
+function Portfolio() {
+  return (
+    <div>
+      <h1>My Portfolio</h1>
+      <p>This is where I will showcase my projects and skills.</p>
+    </div>
+  );
+}
+
+export default Portfolio;
