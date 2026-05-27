c350bdbe44a5d727623fef9adbb23dc94a4f7541
Lương Trọng Hiếu <luongtronghieudz06@gmail.com>
2026-05-27 11:58:06 +0700

    [COMPONENT] Create Skills component

---
+++ session_04_react_basics/projects/portfolio_react/src/components/Skills.jsx
@@ -0,0 +1,13 @@
+function Skills() {
+    return (
+        <div>
+            <h2>Skills</h2>
+            <ul>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>CSS</li>
+                <li>HTML</li>
+            </ul>
+        </div>
+    );
+}
