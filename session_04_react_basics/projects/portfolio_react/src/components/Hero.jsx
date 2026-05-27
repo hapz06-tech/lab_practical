dff2e2ea6c13188872e48370065fa7347f77bffa
Lương Trọng Hiếu <luongtronghieudz06@gmail.com>
2026-05-27 11:49:41 +0700

    [COMPONENT] Create Hero component

---
+++ session_04_react_basics/projects/portfolio_react/src/components/Hero.jsx
@@ -0,0 +1,12 @@
+function Hero() {
+    return (
+        <section className="hero">
+            <div className="container">
+                <h1>Welcome to My Portfolio</h1>
+                <p>I'm a passionate developer creating amazing web experiences.</p>
+            </div>
+        </section>
+    )
+}
+
+export default Hero;
